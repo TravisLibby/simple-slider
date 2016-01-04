0b81013a22ee1c3bb7af8b85a30b697c02a02b5e
Travis <travis.libby@xero.com>
2016-01-03 20:25:24 -0800

    Remove previous arrow if at beginning of slideshow.

--- js/main.js
+++ js/main.js
@@ -4,22 +4,32 @@ var currLeftPos = 0;
 var minLeftPos = 0;
 var maxLeftPos = -800;
 
+// Hide prev arrow to start
+$('#prev').hide();
+
+// Prev Button
 $('#prev').on('click', function() {
   if (!isAnimating() && currLeftPos !== minLeftPos) {
     $('#slider').animate({
       left: '+=400'
     }, speed, function() {
       currLeftPos += 400;
+      if (currLeftPos === minLeftPos) {
+        $('#prev').hide();
+      }
     });
   }
 });
 
+// Next Button
 $('#next').on('click', function() {
+  $('#prev').show();
   if (!isAnimating() && currLeftPos === maxLeftPos) {
     $('#slider').animate({
       left: '+=800'
     }, speed, function() {
       currLeftPos = 0;
+      $('#prev').hide();
     });
   }
   if (!isAnimating() && currLeftPos !== maxLeftPos) {
